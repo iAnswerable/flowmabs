63435a328a5ba7a9db2045e6329478b1b60a7ae6
Gopal <gopal.rohila17@gmail.com>
2025-02-11 15:52:58 +0530

    Update layout.component.ts

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-layout',
